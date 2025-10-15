6443ae49cf1ed7eea421022dbf6c04d0dd2c5f89
CrisFe <cristhianfernandoholguin@gmail.com>
2025-10-15 14:51:30 -0500

    clases para las estructuras de datos

--- Fase3CristhianQuintero.py
+++ Fase3CristhianQuintero.py
@@ -26,4 +26,25 @@ class Pila:
     
 class Cola:
     #--- Implementación de una cola(FIFO) ---
+    def __init__(self):
+        self.items = deque()
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.popleft()
+        return None
     
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+
+class Nodo:
+    #--- Nodo para la lista enlazada ---
+    def __init__(self,dato):
+        self.dato = dato
+        self.siguiente = None
